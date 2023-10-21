6a97d8ea29d4f8ab4321f7143549f189d9f2fc04
rishavmngo <rishavinmngo@gmail.com>
2023-10-21 14:09:32 +0530

    fixing style of hero component

--- components/hero.component.tsx
+++ components/hero.component.tsx
@@ -11,7 +11,7 @@ export default function HeroSection() {
                 <h1 className="relative sm:text-8xl md:text-9xl text-7xl z-20 ">
                     Rishav Raj
                     <motion.div
-                        className="absolute rounded-full -right-6 -top-2 -z-20 w-[80px] h-[80px] bg-[#B87650] max-[640px]:left-[125px] max-[640px]:top-[120px]"
+                        className="absolute rounded-full -right-6 -top-2 -z-20 w-[80px] h-[80px] bg-[#B87650] max-[640px]:left-[125px] max-[640px]:top-[120px] max-[640px]:hidden"
                         // initial={{ opacity: 0, scale: 0, y: 500 }}
                         // animate={{ opacity: 1, scale: 1, y: 0 }}
 
@@ -38,11 +38,11 @@ export default function HeroSection() {
             <a
                 href="/resume.pdf"
                 download={true}
-                className=" mb-[1.8rem] sm:mb-[6.8rem] text-[#656565] dark:text-white p-1 px-2 rounded border border-[1.9px] border-[#656565] cursor-pointer hover:bg-[#B87650] hover:text-gray-200 active:bg-[#A8653F]"
+                className="mt-[4rem] mb-[5.5rem] sm:mb-[6.8rem] text-[#656565] dark:text-white p-1 px-2 rounded border border-[1.9px] border-[#656565] cursor-pointer hover:bg-[#B87650] hover:text-gray-200 active:bg-[#A8653F]"
             >
                 Resume
             </a>
-            <div className="text-[#363636] dark:text-white text-4xl">
+            <div className="text-[#363636] dark:text-white text-2xl sm:text-4xl">
                 <h4>
                     <span className="text-[#B87650]">Developer</span> on duty
                 </h4>
